6a5fa1711cc53c6cc1d19e7d701416aa797cf616
Seldszar <contact@seldszar.fr>
2016-08-29 14:18:32 +0200

    Fixed wrong command

--- 2.4/docker-entrypoint.sh
+++ 2.4/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'icecast2' ]; then
+if [ "$1" = 'icecast' ]; then
     perl -pi -e 's/\$\{(\w+)\}/$ENV{$1}/g' /etc/icecast2/icecast.xml
 
     exec gosu icecast2 "$@"
